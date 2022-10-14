9ce0150b357896dc0c92db5728cbcc3147ab9149
tegeba01 <tegeba01@n22636.PAD.PTB.DE>
2022-10-14 11:39:10 +0200

    Wuerfel und Pyramide Klasse

--- src/de/ostfalia/prog/s22ws/model/Pyramide.java
+++ src/de/ostfalia/prog/s22ws/model/Pyramide.java
@@ -1,5 +1,48 @@
 package de.ostfalia.prog.s22ws.model;
 
-public class Pyramide {
+import de.ostfalia.prog.s22ws.base.Farbe;
 
-}
+public class Pyramide {
+	 static Pyramide pyramideInstance;
+	    private Wuerfel[] wuerfelliste = new Wuerfel[5];
+
+	    private Pyramide() {
+	        pyramideInstance = this;
+	    }
+
+	    public static Pyramide getInstance() {
+	        if (pyramideInstance == null) {
+	            pyramideInstance = new Pyramide();
+	        }
+
+	        return pyramideInstance;
+
+	    }
+
+	    public void initialisiere(String... wuerfel) {
+	        int augenzahl;
+	        for (int i = 0; i < wuerfel.length; i++) {
+	            String[] str = wuerfel[i].split("\\:");
+	            augenzahl = Integer.parseInt(str[1]);
+	            for (Farbe f : Farbe.values()) {
+	                String s = f.toString();
+	                if (s.equals(str[0])) {
+	                    wuerfelliste[i] = new Wuerfel(f, augenzahl);
+	                }
+	            }
+	        }
+	    }
+
+	    public int anzahlWuerfel() {
+	        return 0;
+	    }
+
+	    public String toString() {
+	        String ausgabe = "";
+	        for (int i = 0; i < wuerfelliste.length; i++) {
+	            ausgabe += wuerfelliste[i] + "\n";
+	        }
+	        return ausgabe;
+	    }
+	}
+
---
+++ src/de/ostfalia/prog/s22ws/model/Wuerfel.java
@@ -0,0 +1,23 @@
+package de.ostfalia.prog.s22ws.model;
+
+import de.ostfalia.prog.s22ws.base.Farbe;
+
+public class Wuerfel {
+
+private Farbe farbe;
+private int augenzahl;
+
+public Wuerfel( Farbe farbe, int augenzahl) {
+        this.farbe = farbe;
+        this.augenzahl = augenzahl;
+}
+
+@Override
+public String toString() {
+    return " farbe=" + farbe + ", augenzahl=" + augenzahl;
+}
+
+
+}
+
+
